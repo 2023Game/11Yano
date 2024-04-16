6bd5ce59b541355597322eaa85e8824c2183f1d9
Yano-Shiori-Fukuyama <gc23011@anabuki-net.ne.jp>
2024-04-16 10:40:07 +0900

    00

--- 3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
@@ -11,10 +11,10 @@
 #include "CBillBoard.h"
 #include "CColliderLine.h"
 #include "CEnemy3.h"
+#include "CCamera.h"
 
 //クラスのstatic変数
 CTexture CApplication::mTexture;
-CCharacterManager CApplication::mCharacterManager;
 
 #define SOUND_BGM "res\\mario.wav" //BGM音声ファイル
 #define SOUND_OVER "res\\mdai.wav" //ゲームオーバー音声ファイル
@@ -22,96 +22,44 @@ CCharacterManager CApplication::mCharacterManager;
 #define MODEL_BACKGROUND "res\\sky.obj", "res\\sky.mtl"
 #define MODEL_C5 "res\\c5.obj","res\\c5.mtl"//敵輸送機モデル
 
-CCharacterManager* CApplication::CharacterManager()
-{
-	return &mCharacterManager;
-}
 
 CTexture* CApplication::Texture()
 {
 	return &mTexture;
 }
 
+
 void CApplication::Start()
 {
-	mModelC5.Load(MODEL_C5);
-	new CEnemy(&mModelC5, CVector(0.0f, 10.0f, -100.0f),
-		CVector(), CVector(0.1f, 0.1f, 0.1f));
-	new CEnemy(&mModelC5, CVector(30.0f, 10.0f, -130.0f),
-		CVector(), CVector(0.1f, 0.1f, 0.1f));
-	new CEnemy3(CVector(-5.0f, 1.0f, -10.0f), CVector(), CVector(0.1f, 0.1f, 0.1f));
-	new CEnemy3(CVector(5.0f, 1.0f, -10.0f), CVector(), CVector(0.1f, 0.1f, 0.1f));
-	mPlayer.Model(&mModel);
-	mPlayer.Scale(CVector(0.1f, 0.1f, 0.1f));
-	mPlayer.Position(CVector(0.0f, 0.0f, -3.0f));
-	mPlayer.Rotation(CVector(0.0f,180.0f,0.0f));
-	CMatrix matrix;
-	matrix.Point();
-	mBackGround.Load(MODEL_BACKGROUND);
-	mEye = CVector(1.0f, 2.0f, 3.0f);
-	mModel.Load(MODEL_OBJ);
-	//ビルボードの作成
-	new CBillBoard(CVector(-6.0f, 3.0f, -10.0f), 1.0f, 1.0f);
-    mColliderMesh.Set(nullptr, nullptr, &mBackGround);
-	spUi = new CUi();//UIクラスの生成
+	mFont.Load("FontG.png", 1, 4096 / 64);
 }
 
 void CApplication::Update()
 {
-	CTaskManager::Instance()->Update();
-	//CCollisionManager::Instance()->Collision();
-	CTaskManager::Instance()->Collision();
-	CVector v0, v1, v2, n; //頂点1，2，3，法線データの作成
-	n.Set(0.0f, 1.0f, 0.0f); //法線を上向きで設定する
-	v0.Set(0.0f, 0.0f, 0.5f); //頂点1の座標
-	v1.Set(1.0f, 0.0f, 0.0f); //頂点2の座標
-	v2.Set(0.0f, 0.0f, -0.5f); //頂点3の座標
-
-	if (mInput.Key('J'))
-	{
-		mEye = mEye - CVector(0.1f, 0.0f, 0.0f);
-	}
-	if (mInput.Key('L'))
-	{
-		mEye = mEye + CVector(0.1f, 0.0f, 0.0f);
-	}
-	if (mInput.Key('I'))
-	{
-		mEye = mEye - CVector(0.0f, 0.0f, 0.1f);
-	}
-	if (mInput.Key('K'))
-	{
-		mEye = mEye + CVector(0.0f, 0.0f, 0.1f);
-	}
-	if (mInput.Key('O'))
-	{
-		mEye = mEye - CVector(0.0f, 0.1f, 0.0f);
-	}
-	if (mInput.Key('M'))
-	{
-		mEye = mEye + CVector(0.0f, 0.1f, 0.0f);
-	}
-
-	CVector e, c, u; //視点、注意点、上方向
-	e = mPlayer.Position() + CVector(-0.2f, 1.0f, -3.0f) * mPlayer.MatrixRotate();
-	c = mPlayer.Position();
-	u = CVector(0.0f, 1.0f, 0.0f) * mPlayer.MatrixRotate();
-	//カメラ設定
+	//カメラのパラメータを作成する
+	CVector e, c, u;//視点、注意点、上方向
+	//視点を求める
+	e = CVector(1.0f, 2.0f, 10.0f);
+	//注意点を求める
+	c = CVector();
+	//上方向を求める
+	u = CVector(0.0f, 1.0f, 0.0f);
+	//カメラの設定
 	gluLookAt(e.X(), e.Y(), e.Z(), c.X(), c.Y(), c.Z(), u.X(), u.Y(), u.Z());
-	//モデルビュー行列取得
+	//モデルビュー行列の取得
 	glGetFloatv(GL_MODELVIEW_MATRIX, mModelViewInverse.M());
 	//逆行列の取得
 	mModelViewInverse = mModelViewInverse.Transpose();
 	mModelViewInverse.M(0, 3, 0);
 	mModelViewInverse.M(1, 3, 0);
 	mModelViewInverse.M(2, 3, 0);
-	
-	CTaskManager::Instance()->Delete();
-	mBackGround.Render();
-	CTaskManager::Instance()->Render();
-	CCollisionManager::Instance()->Render();
-	spUi->Render();//UIの描画
-	
+	//2D描画開始
+	CCamera::Start(0, 800, 0, 600);
+
+	mFont.Draw(20, 20, 10, 12, "3D PROGRAMMING");
+
+	//2Dの描画終了
+	CCamera::End();
 }
 
 CMatrix CApplication::mModelViewInverse;
--- 3DLv1_vs2019_00/GameProgramming/src/CApplication.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CApplication.h
@@ -1,16 +1,11 @@
 #pragma once
-#include "CRectangle.h"
 #include "CTexture.h"
-#include "CCharacter.h"
 #include "CBullet.h"
 #include "CEnemy.h"
 #include "CPlayer.h"
 #include "CInput.h"
 #include "CFont.h"
-#include "CMiss.h"
 #include <vector>
-#include "CCharacterManager.h"
-#include "CGame.h"
 #include "CSound.h"
 #include "CVector.h"
 #include "CModel.h"
@@ -34,8 +29,6 @@ private:
 	CVector mEye;
 	CSound mSoundBgm;	//BGM
 	CSound mSoundOver;	//ゲームオーバー
-	CGame* mpGame;
-	static CCharacterManager mCharacterManager;
 	enum class EState
 	{
 		ESTART,	//ゲーム開始
@@ -51,14 +44,12 @@ private:
 //	CBullet* mpBullet;
 	CInput mInput;
 	CFont mFont;
-	CMiss* mpMiss;
 	//CCharacterのポインタの可変長配列
 //	std::vector<CCharacter*> mCharacters;
 public:
 	~CApplication();
 	static CUi* Ui();//UIクラスのインスタンス取得
 	static const CMatrix& ModelViewInverse();
-	static CCharacterManager* CharacterManager();
 	static CTexture* Texture();
 	//最初に一度だけ実行するプログラム
 	void Start();
